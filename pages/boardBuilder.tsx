149382b662888cf6cc88bd8d6e74c4de5f592552
Micah Gideon Modell <mgmodell@users.noreply.github.com>
2023-03-20 02:44:48 +0000

    I can now select a cell

--- pages/boardBuilder.tsx
+++ pages/boardBuilder.tsx
@@ -7,19 +7,25 @@ enum Orientation {
   VERTICAL=2
 }
 enum Enhancement {
+  NA = 'NA',
   L2 = 'L2',
   L3 = 'L3',
   W2 = 'W2',
   W3 = 'W3'
 };
 type Cell = {
+  xPos: number;
+  yPos: number;
   mine: boolean;
+  focused: boolean;
   enhancement: Enhancement;
   letter: string;
 };
 type Board = {
   rows: {
-    [key: number]: Cell;
+    [key: number]: {
+      [key: number]: Cell;
+    }
   }
 }
 
@@ -33,15 +39,34 @@ type PlacedWord = {
 
 export default function BoardBuilder(props){
 
-  const [grid,setGrid] = useState<Board | undefined>(undefined);
+  const [grid,setGrid] = useState<Board>({});
   const [placedWords, setPlacedWords] = useState(Array<PlacedWord>)
   const [baseWords, setBaseWords] = useState([] );
+  const [curCell, setCurCell] = useState<Cell>()
+  
   const initBoard = () => {
+    const tmp_grid:Board = {};
+    const x = 16;
+    const y = 16;
+    for( let x_pos = 0; x_pos < x; x_pos ++  ){
+      tmp_grid[x_pos] = {};
+      for( let y_pos = 0; y_pos < y; y_pos ++  ){
+        tmp_grid[x_pos][y_pos] ={
+          xPos: x_pos,
+          yPos: y_pos,
+          mine: false,
+          focused: false,
+          enhancement: Enhancement.NA,
+          letter: ''
+        }
+      }
+    }
+    setGrid( tmp_grid );
+    //Now let's get some data
     const url = 'https://raw.githubusercontent.com/' +
       'mgmodell/corpora/master/data/words/common.json';
          axios.get(url )
           .then((resp) =>{
-            console.log( resp );
             const words = resp.data.commonWords;
             console.log( words.length );
             setBaseWords( words );
@@ -62,28 +87,62 @@ export default function BoardBuilder(props){
       })
     }
     setPlacedWords( tempWords );
-    console.log( tempWords );
     
   }
   useEffect(()=>{
     placeWords( );
   },[baseWords]);
-  
-  const board = useMemo( () =>{
-    const output = [];
-    const x = 16;
-    for( let index = 0; index < x * x; index ++  ){
-      output.push(
-        <div key={index}>{index}</div>
-      )
-    }
-    return(
-      <div className={styles.board}>
-        {output}
-      </div>
-    )
+
+  const selectCell = (event) => {
+    const xPos = parseInt( event.target.attributes.xpos.value );
+    const yPos = parseInt( event.target.attributes.ypos.value );
+    const tmp_grid = Object.assign( {}, grid );
     
-  }, [] );
+    console.log( curCell );
+    if( curCell != undefined ){
+      const prevCell = Object.assign({}, curCell);
+      prevCell.focused = false;
+      tmp_grid[prevCell.xPos][prevCell.yPos] = prevCell;
+    }
+    
+    let cur_cell:Cell = tmp_grid[xPos][yPos];
+    
+    cur_cell.focused = true;
+    setGrid( tmp_grid );
+    setCurCell( cur_cell );
+    
+  }
+  const board = useMemo( () =>{
+    const x = 16;
+    const y = 16;
+    
+    if( Object.keys(grid).length < x ){
+      return null;
+    } else {
+      const output = [];
+      for( let x_pos = 0; x_pos < x; x_pos ++  ){
+        for( let y_pos = 0; y_pos < y; y_pos ++  ){
+          const cur_pos = (x_pos * x ) + y_pos;
+          const cur_cell = grid[x_pos][y_pos];
+          output.push(
+            <div key={cur_pos}
+              className={cur_cell.focused ? styles.current : styles.inactive }
+              xpos={x_pos}
+              ypos={y_pos}
+              onClick={selectCell}
+              >{cur_cell.letter.length > 0 ? cur_cell.letter : '-'}</div>
+          )
+        }
+      }
+      return(
+        <div className={styles.board}>
+          {output}
+        </div>
+      )
+      
+    }
+    
+  }, [grid] );
 
   return(
     <Fragment>
