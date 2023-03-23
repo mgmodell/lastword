dd75d702f671b596fd73e8480cf062ef0e9c4ef6
Micah Gideon Modell <mgmodell@users.noreply.github.com>
2023-03-23 17:26:46 +0000

    First word placement.

--- pages/boardBuilder.tsx
+++ pages/boardBuilder.tsx
@@ -40,14 +40,19 @@ type PlacedWord = {
 export default function BoardBuilder(props){
 
   const [grid,setGrid] = useState<Board>({});
-  const [placedWords, setPlacedWords] = useState(Array<PlacedWord>)
+  const [placedWords, setPlacedWords] = useState(Array<PlacedWord>);
   const [baseWords, setBaseWords] = useState([] );
   const [curCell, setCurCell] = useState<Cell>()
+  const [usedCells, setUsedCells] = useState(Array<Cell>);
+
+  const GRID_SIZE = 16;
+  const x = GRID_SIZE;
+  const y = GRID_SIZE;
   
   const initBoard = () => {
+    setPlacedWords( [] );
+    setUsedCells( [] );
     const tmp_grid:Board = {};
-    const x = 16;
-    const y = 16;
     for( let x_pos = 0; x_pos < x; x_pos ++  ){
       tmp_grid[x_pos] = {};
       for( let y_pos = 0; y_pos < y; y_pos ++  ){
@@ -62,37 +67,100 @@ export default function BoardBuilder(props){
       }
     }
     setGrid( tmp_grid );
+  }
+  const fetchWords = ()=>{
     //Now let's get some data
     const url = 'https://raw.githubusercontent.com/' +
       'mgmodell/corpora/master/data/words/common.json';
          axios.get(url )
           .then((resp) =>{
-            const words = resp.data.commonWords;
-            console.log( words.length );
+            const words = resp.data.commonWords.filter(word => word.length < 9 );
             setBaseWords( words );
           })
-  }
-
-  const placeWords = ()=>{
-    const count = 10 + (Math.random() * 20);
-    const tempWords:Array<PlacedWord> = [];
-    for( let index = 0; index < count; index++ ){
-      const place = Math.round( baseWords.length * Math.random() );
-      tempWords.push({
-        x: 1,
-        y: 1,
-        orientation: Orientation.HORIZONTAL,
-        word: baseWords[ place ],
-        place: place
-      })
-    }
-    setPlacedWords( tempWords );
     
   }
-  useEffect(()=>{
-    placeWords( );
-  },[baseWords]);
 
+  useEffect( ()=>{
+    initBoard( );
+    fetchWords( );
+    
+  },[])
+
+  const placeWords = ()=>{
+    initBoard( );
+    const count = 10 + (Math.random() * 20);
+    const tmpWords:Array<PlacedWord> = [];
+    
+    const tmpUsedCells = [...usedCells];
+    for( let index = 0; index < count; index++ ){
+      const place = Math.round( baseWords.length * Math.random() );
+      const proposedWord = baseWords[ place ];
+
+      let placedWord:PlacedWord = {};
+      if( tmpUsedCells.length < 1 ){
+        const orientation = Math.round( Math.random() * 2 );
+        const hiLow = Math.round( Math.random() * 2 );
+        const position = Math.round( Math.random() * GRID_SIZE );
+        placedWord = {
+          orientation: orientation,
+          word: proposedWord,
+          place: place
+        }
+        let offset = 0;
+        console.log( placedWord );
+        if( hiLow === 2 ){
+          offset = GRID_SIZE - placedWord.word.length;
+        }
+        if( Orientation.HORIZONTAL !== placedWord.orientation ){
+          placedWord.x = offset;
+          placedWord.y = position;
+        } else {
+          placedWord.y = offset;
+          placedWord.x = position;
+        }
+        
+      } else {
+        
+      }
+      if( isValidPlacement( placedWord ) ){
+        tmpWords.push(placedWord );
+        setPlacement( placedWord, tmpUsedCells );
+      }
+    }
+    setPlacedWords( tmpWords );
+    
+  }
+  const setPlacement = ( placedWord:PlacedWord, usedCellsStore:Array<Cell>,  ) =>{
+        const tmpGrid:Board = Object.assign( {}, grid );
+        let curX = placedWord.x;
+        let curY = placedWord.y;
+        let tmpCell = tmpGrid[curX][curY];
+        cell.letter = placedWord.word.charAt( 0 );
+        cell.mine = false;
+        usedCellsStore.push( tmpCell );
+        
+        for( let curChar = 1; curChar < placedWord.word.length; curChar++ ){
+          if( Orientation.HORIZONTAL === placedWord.orientation ){
+            curX = placedWord.x;
+            curY = placedWord.y + curChar;
+          } else {
+            curX = placedWord.x + curChar;
+            curY = placedWord.y;
+            
+          }
+          cell = tmpGrid[curX][curY];
+          cell.letter = placedWord.word.charAt( curChar );
+          cell.mine = false;
+          usedCellsStore.push( tmpCell );
+        }
+        setGrid( tmpGrid );
+        setUsedCells( tmpUsedCells );
+    
+  }
+  const isValidPlacement = ( placedWord:PlacedWord ) =>{
+    return true;
+    
+  }
   const selectCell = (event) => {
     const xPos = parseInt( event.target.attributes.xpos.value );
     const yPos = parseInt( event.target.attributes.ypos.value );
@@ -112,6 +180,10 @@ export default function BoardBuilder(props){
     setCurCell( cur_cell );
     
   }
+  const selectWords = useMemo( () =>{
+    return baseWords.filter( (word) => /[xzqwvkgfyb]/.test( word ) )
+  }, [baseWords])
+  
   const board = useMemo( () =>{
     const x = 16;
     const y = 16;
@@ -146,7 +218,7 @@ export default function BoardBuilder(props){
 
   return(
     <Fragment>
-      <button onClick={initBoard} >New Board</button>
+      <button onClick={placeWords} >New Board</button>
       {board}
     </Fragment>
   )
