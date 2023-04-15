71f64b3cd999a905f5d25624f1d442c9ff7bb69e
Micah Gideon Modell <micah.modell@gmail.com>
2023-04-15 16:43:27 -0400

    prettier

--- pages/api/RandomGenerators.ts
+++ pages/api/RandomGenerators.ts
@@ -1,28 +1,24 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import axios from 'axios'
+import axios from "axios";
 
-const NAME_URL = 
-    'https://genr8rs.com/api/Content/Fun/MafiaNameGenerator?genr8rsUserId=1681049742.31146432c88e4c06e&_sT=1';
-const TAUNT_URL = 
-    'https://genr8rs.com/api/Content/Fun/GameTauntGenerator?genr8rsUserId=1681049742.31146432c88e4c06e&_sInsultLevel=polite';
+const NAME_URL =
+  "https://genr8rs.com/api/Content/Fun/MafiaNameGenerator?genr8rsUserId=1681049742.31146432c88e4c06e&_sT=1";
+const TAUNT_URL =
+  "https://genr8rs.com/api/Content/Fun/GameTauntGenerator?genr8rsUserId=1681049742.31146432c88e4c06e&_sInsultLevel=polite";
 
 interface RandomContentCallback {
-  (name: string) : void;
+  (name: string): void;
 }
 
-export function RandomName( callback: RandomContentCallback ):string{
-    axios.get(NAME_URL )
-      .then((resp) =>{
-        const receivedName = resp.data._sResult;
-        callback( receivedName );
-      })
-
+export function RandomName(callback: RandomContentCallback): string {
+  axios.get(NAME_URL).then(resp => {
+    const receivedName = resp.data._sResult;
+    callback(receivedName);
+  });
 }
-export function RandomTaunt( callback: RandomContentCallback ):string{
-    axios.get(TAUNT_URL )
-      .then((resp) =>{
-        const receivedTant = resp.data._sResult;
-        callback( receivedTant );
-      })
-
+export function RandomTaunt(callback: RandomContentCallback): string {
+  axios.get(TAUNT_URL).then(resp => {
+    const receivedTant = resp.data._sResult;
+    callback(receivedTant);
+  });
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,16 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import BoardBuilder from './boardBuilder';
-import RandomComponent from './randomComponent';
-
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import BoardBuilder from "./boardBuilder";
+import RandomComponent from "./randomComponent";
 
 export default function Home() {
   return (
-    <div >
+    <div>
       <Head>
         <title>The Last Word</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-        <BoardBuilder />
-
+      <BoardBuilder />
     </div>
-  )
+  );
 }
