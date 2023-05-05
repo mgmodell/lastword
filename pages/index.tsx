8198b1eff3eb8608e5106a64bcc9fa93901d6b8f
Micah Gideon Modell <micah.modell@gmail.com>
2023-05-05 14:46:43 -0400

    Hideable help

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,37 @@
 import Head from "next/head";
+
+import { getCookie, setCookie, hasCookie } from "cookies-next";
+
 import styles from "../styles/Home.module.css";
 import BoardBuilder from "./boardBuilder";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  return (
-    <div>
-      <Head>
-        <title>The Last Word</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <h2>The game is a <del>a foot</del> afoot and there are no tiles left but what&apos;s in your hand. Can you play a word to beat your challenger?</h2>
+  const HELP_COOKIE = 'show-lastword-help';
+  const [showHelp, setShowHelp] = useState( true );
+
+  useEffect( () => {
+    if( !hasCookie( HELP_COOKIE ) ){
+      setShowHelp( true );
+    } else {
+      setShowHelp( Boolean( getCookie( HELP_COOKIE ) ) );
+    }
+
+  },[])
+  
+  const toggleHelp = () => {
+    let newVal = false;
+    if( hasCookie( HELP_COOKIE ) ){
+      newVal = ! Boolean( getCookie( HELP_COOKIE ) );
+    }
+    setShowHelp( newVal );
+    setCookie( HELP_COOKIE, newVal );
+
+  };
+
+  const helpContent = showHelp ? (
+    <>
+      <button onClick={toggleHelp} >Hide this help</button>
       <ol>
         <li>Click <i>bring the next challenger</i> to reveal the game board.</li>
         <li>Review the board and your tiles and play the best word to close the gap in scores and beat the challenger.</li>
@@ -23,6 +45,19 @@ export default function Home() {
           </ol>
         </li>
       </ol>
+    </>
+  ) : (
+    <button onClick={toggleHelp} >Click for help</button>
+  )
+
+  return (
+    <div>
+      <Head>
+        <title>The Last Word</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h2>The game is a <del>a foot</del> afoot and there are no tiles left but what&apos;s in your hand. Can you play a word to beat your challenger?</h2>
+      {helpContent}
       <BoardBuilder />
     </div>
   );
